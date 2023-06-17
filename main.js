522b6df630313fe6a6acb6d98168391b9df7a373
Morgan Crawford <macrawfo94@gmail.com>
2023-06-16 17:10:34 -0700

    some message

--- main.js
+++ main.js
@@ -39,21 +39,49 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        // console.log(data)
         data.cards.forEach(card =>{
           schemeDeck.push(card)
           
         })
         //!: This function puts the images onto my HTML page!
+        //?Each card comes with a 'id' from the API, I can use this to check and see which one is clicked. I need to be able to push the ID and the image all together and make sure that they are transferred. I can use the ID for each element to keep track of them and where they are going. That will be their 'flag'
 
+        //TODO: If more that 2 elements with the same id maybe share an error and say you can only have 2?
+        console.log(schemeDeck)
         for(let i=0;i<schemeDeck.length;i++){
+          
           const listOfSchemes = document.getElementById("listOfSchemes")
           const li = document.createElement('li')
           const img = document.createElement('img')
+
+          //takes the ID from the card data and carrys it with the image. This way I can tell which image was clicked etc. This can console log the actual ID
+          img.dataset.id = schemeDeck[i].id
+
           //appending the image
           img.src = schemeDeck[i].imageUrl
           img.className = 'cardImage'
           li.appendChild(img)
           listOfSchemes.appendChild(li)
+
+
+          //*ChatGPT idk what this is. This is 
+            // Creating the dropdown options
+            // for (let count = 0; count <= 2; count++) {
+            //   const option = document.createElement('option');
+            //   option.value = count;
+            //   option.text = count;
+            //   select.appendChild(option);
+
+            //   select.addEventListener('change', function(e) {
+            //     const selectedCount = parseInt(e.target.value);
+            //     addToDeck(schemeDeck[i], selectedCount);
+            //   });
+              
+            //   li.appendChild(select);
+            //   listOfSchemes.appendChild(li);
+  // }
+
         }
 
           
@@ -61,9 +89,11 @@ function getFetch(){
             items.forEach((item) => {
             item.addEventListener('click', function() {
             // Event listener logic
-            console.log('Item clicked:', item.textContent);
-            // Additional code to handle the click event
+            //This brings out the the img src.
+            console.log('Item clicked:', item);
+            // Additional code to handle the click event that adds the cards to the deck
   });
+  
 });
 
       })
